6b9d3354fbb579bd40becc033bd8e770fe328081
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-10-26 20:01:09 +0300

    HW 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
+        Data data = new Data("dew", "a", "a");
+        data.printInfo();
 
     }
 }
